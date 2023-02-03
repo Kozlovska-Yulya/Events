cb0a878a3e976e00933871f97244cdc3fc9ec2c7
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-02-03 16:25:06 +0100

    click span

--- Task-1/index.js
+++ Task-1/index.js
@@ -1,6 +1,12 @@
+const spanElem = document.querySelector('rect_span');
+
 const logTarget = (text, color) => {
   const eventListElem = document.querySelector('.events-list');
   eventListElem.innerHTML = (
     <span style="color: grey; margin-left: 8px;">div</span>
   );
 };
+
+const logGreenSpan = logTarget.bind(null, 'Span', 'green');
+
+elem.addEvenListener('click', logGreenSpan);
