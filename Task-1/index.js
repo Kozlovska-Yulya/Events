f24680fa368a4499feac52620783b26c2244ccee
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-02-03 15:24:36 +0100

    added element span

---
+++ Task-1/index.js
@@ -0,0 +1,6 @@
+const logTarget = (text, color) => {
+  const eventListElem = document.querySelector('.events-list');
+  eventListElem.innerHTML = (
+    <span style="color: grey; margin-left: 8px;">div</span>
+  );
+};
