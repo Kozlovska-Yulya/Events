2c06e458114d50191e5df3edfb8cbd05d35ae7eb
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-02-03 20:20:07 +0100

    divEl, pEl, spanEl

--- Task-1/index.js
+++ Task-1/index.js
@@ -1,12 +1,25 @@
+const divElem = document.querySelector('rect_div');
+const pElem = document.querySelector('rect_p');
 const spanElem = document.querySelector('rect_span');
 
 const logTarget = (text, color) => {
   const eventListElem = document.querySelector('.events-list');
-  eventListElem.innerHTML = (
-    <span style="color: grey; margin-left: 8px;">div</span>
-  );
+  eventListElem.innerHTML += `<span style="color: grey; margin-left: 8px;">div</span>`;
 };
 
+const logGreenDiv = logTarget.bind(null, 'Div', 'green');
+const logGreenP = logTarget.bind(null, 'P', 'green');
 const logGreenSpan = logTarget.bind(null, 'Span', 'green');
 
-spanElem.addEvenListener('click', logGreenSpan);
+const logGreyDiv = logTarget.bind(null, 'Div', 'grey');
+const logGreyP = logTarget.bind(null, 'P', 'grey');
+const logGreySpan = logTarget.bind(null, 'Span', 'grey');
+
+divElem.addEventListener('click', logGreenDiv);
+divElem.addEventListener('click', logGreyDiv, true);
+
+pElem.addEventListener('click', logGreenP);
+pElem.addEventListener('click', logGreyP, true);
+
+spanElem.addEventListener('click', logGreenSpan);
+spanElem.addEventListener('click', logGreySpan, true);
