e5dc5bbca09624ac14ee2e26e52381c67d65c6a6
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-02-04 00:07:51 +0100

    add and remove handle click on button element

---
+++ Task-6/index.js
@@ -0,0 +1,6 @@
+const buttonElem = document.querySelector('.single-use-btn');
+function handleClick() {
+  console.log('clicked');
+  buttonElem.removeEventListener('click', handleClick);
+}
+buttonElem.addEventListener('click', handleClick);
