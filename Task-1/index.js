9609e93df0a8ac2f2b0feef7eb50f25971d56a3c
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-02-07 16:19:54 +0100

    added div, p, span

--- Task-1/index.js
+++ Task-1/index.js
@@ -1,9 +1,18 @@
+const divElem = document.querySelector('.rect_div');
+const pElem = document.querySelector('.rect_p');
 const spanElem = document.querySelector('.rect_span');
 
 const logTarget = () => {
   const eventsListElem = document.querySelector('.events-list');
+  eventsListElem.innerHTML += `<span style="color: grey; margin-left: 8px">div</span>`;
+  eventsListElem.innerHTML += `<span style="color: grey; margin-left: 8px">p</span>`;
   eventsListElem.innerHTML += `<span style="color: grey; margin-left: 8px">span</span>`;
-  eventsListElem.innerHTML += `<span style="color: green; margin-left: 8px">span</span>`;
 };
 
-spanElem.addEventListener('click', logTarget);
+const logGreyDiv = logTarget.bind();
+const logGreyP = logTarget.bind();
+const logGreySpan = logTarget.bind();
+
+divElem.addEventListener('click', logGreyDiv);
+pElem.addEventListener('click', logGreyP);
+pElem.addEventListener('click', logGreySpan);
